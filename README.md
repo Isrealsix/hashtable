845aff22494b1130c6ea58c91f3eebef0f5a88a7
Isreal6ix <isrealsix@gmail.com>
2023-11-04 03:06:16 +0700

    VAgitus

---
+++ README.md
@@ -0,0 +1,2 @@
+# hashtable-java
+Vector containing list headers (Hash table) with java
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        MyConsole c = new MyConsole();
+    }
+}
